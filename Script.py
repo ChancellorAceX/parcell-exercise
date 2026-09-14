d3712af4a76547c88a2e56c0bd42174af41ec749
Adam Brown <adam.edward.brown@gmail.com>
2026-09-13 20:27:41 -0400

    completed task (over time limit)

--- Script.py
+++ Script.py
@@ -1,7 +1,10 @@
 # Script
 import re
 import requests
+import json
 url = 'https://ftis.org/'
+
+
 response = requests.get(url+'Posts.aspx')
 if response.status_code == 200:
     print('response received')
@@ -23,7 +26,29 @@ for id in IDs:
 links = []
 for id in GoodIDs:
     links.append(url+'PostFileDownload.aspx?id='+id)
-print(links)
     
-    
-  
+# TIME LIMIT SEPARATOR
+
+pos = re.finditer('lessons',data)
+indeces = []
+for val in pos:
+  indeces.append(val.start())
+indeces=indeces[1:]
+
+counts = [0]
+pattern = re.compile(r'javascript:return DownloadFile')
+for index, val in enumerate(indeces):
+  if index == len(indeces)-1:
+    count = len(pattern.findall(data,val))
+  else:
+    count = len(pattern.findall(data,val,indeces[index+1]))
+  counts.append(count)
+
+dictionary = {}
+for n, t in enumerate(Titles):
+  dictionary[t]=links[counts[n]:sum(counts[0:n+2])]
+
+with open('config.json','w') as f:
+  json.dump(dictionary,f,indent=4)
+
+print('config.json created using dicitonary data')
